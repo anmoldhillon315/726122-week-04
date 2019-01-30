f8cbc5598475ce55f3041f0273a4861f7b7a77c7
visualstudio <visualstudio@122-PC11>
2019-01-30 11:30:18 -0500

    week 4 . nextvillage

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,8 +14,29 @@ namespace c0726122
         class village
         {
             public village nextVillage;
+            public village previousVillage;
             public string villagename;
             public bool isAstrildeHere = false;
+        }
+        class countryside
+        {
+            village Maple = new village();
+            village Toronto = new village();
+            village Ajax= new village();
+            village First;
+            village Last;
+            public void Launch()
+            {
+                Maple.villagename = "Maple";
+                Maple.nextVillage = Toronto;
+                Maple.previousVillage = null;
+                Toronto.villagename = "Toronto";
+                Toronto.nextVillage = Maple;
+                Toronto.previousVillage = Ajax;
+                Ajax.villagename = "Ajax";
+                Ajax.nextVillage = Toronto;
+                Ajax.previousVillage = null;
+            }
         }
     }
 }
